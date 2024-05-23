6e5f5c14548472230fa8bc4ffdfef3dd7e79192c
Anna Mikhailets <a.mikhailets@inanomo.com>
2024-05-23 14:38:11 +0300

    Fix. Export card

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './components/card/card';
 export * from './context/theme-context';
 export * as globalStyles from './styles/global';
 export {darkToken as darkThemeToken} from './theme-token';
